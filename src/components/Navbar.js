cd2de0baa4cfb74757d50d33b5fa1e082199f16e
Easwara Krishnan S <easwarakrishnan2003@gmail.com>
2025-05-06 10:34:28 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,94 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button, Switch } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Switch,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ darkMode, setDarkMode }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const menuItems = [
+    { text: "Home", to: "/" },
+    { text: "Exchange Rates (Live)", to: "/exchange" },
+    { text: "About", to: "/about" },
+    { text: "Error Page", to: "/error" },
+  ];
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Loan Calculator
-        </Typography>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" component={Link} to="/exchange">
-          Exchange Rates (Live)
-        </Button>
-        <Button color="inherit" component={Link} to="/about">
-          About
-        </Button>
-        <Button color="inherit" component={Link} to="/error">
-          Error Page
-        </Button>
-        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Loan Calculator
+          </Typography>
+
+          {isMobile ? (
+            <>
+              <IconButton color="inherit" edge="end" onClick={toggleDrawer}>
+                <MenuIcon />
+              </IconButton>
+              <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer}>
+                <List sx={{ width: 250 }}>
+                  {menuItems.map((item) => (
+                    <ListItem
+                      button
+                      key={item.text}
+                      component={Link}
+                      to={item.to}
+                      onClick={toggleDrawer}
+                    >
+                      <ListItemText primary={item.text} />
+                    </ListItem>
+                  ))}
+                  <ListItem>
+                    <Switch
+                      checked={darkMode}
+                      onChange={() => setDarkMode(!darkMode)}
+                    />
+                    <Typography variant="body2">Dark Mode</Typography>
+                  </ListItem>
+                </List>
+              </Drawer>
+            </>
+          ) : (
+            <>
+              {menuItems.map((item) => (
+                <Button
+                  key={item.text}
+                  color="inherit"
+                  component={Link}
+                  to={item.to}
+                >
+                  {item.text}
+                </Button>
+              ))}
+              <Switch
+                checked={darkMode}
+                onChange={() => setDarkMode(!darkMode)}
+              />
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
